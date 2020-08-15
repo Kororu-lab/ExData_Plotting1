0d7fb8dbe11763b0120cdd3519ffb91aedbb80cf
Kororu-lab <chch0926@yonsei.ac.kr>
2020-08-15 17:57:05 +0900

    For drawing Q1 plot

---
+++ plot1.R
@@ -0,0 +1,30 @@
+##for common setting
+#loading dplyr
+library(dplyr)
+
+##Reading table with read.table()
+colname <- c("Date", "Time", "GlobalActive", "GlobalReactive", "Voltage", "GlobalIntensity", "Sub1", "Sub2", "Sub3")
+data1 <- read.table("household_power_consumption.txt", sep = ";", col.names = colname, skip = 1)
+
+##Cutting out data between 2007-02-01 and 2007-02-02
+data2 <- filter(data1, Date == "1/2/2007" | Date == "2/2/2007")
+
+data2$time2 <- paste(data2$Date,data2$Time)
+data2$time2 <- strptime(data2$time2, "%d/%m/%Y %H:%M:%S", tz = "UTC")
+
+
+##Changing datatype for $Date/$Time
+data2[,1] <- as.Date(data2$Date, "%d/%m/%Y")
+data2[,2] <- sub(".*\\s+", "",  strptime(data2$Time, "%H:%M:%S", tz = "UTC"))
+
+##Changing other columm into numeric;
+
+for(i in 3:9){
+    data2[,i] <- as.numeric(data2[,i])
+}
+
+###########################################################################
+#Q1
+png(filename = "plot1.png",width = 480, height = 480)
+with(data2, hist(as.numeric(GlobalActive), xlab = "Global Active Power (kilowatts)", ylab = "Frequency", main = "Global Active Power", col = "red"))
+dev.off()
