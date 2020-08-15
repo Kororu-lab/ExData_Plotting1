4a811289d5eda790f20e649126b5b896fb3bb758
Kororu-lab <chch0926@yonsei.ac.kr>
2020-08-15 18:02:24 +0900

    Unnecessary parts eliminated

--- plot1.R
+++ plot1.R
@@ -9,19 +9,16 @@ data1 <- read.table("household_power_consumption.txt", sep = ";", col.names = co
 ##Cutting out data between 2007-02-01 and 2007-02-02
 data2 <- filter(data1, Date == "1/2/2007" | Date == "2/2/2007")
 
+##And adding time order as $time2
 data2$time2 <- paste(data2$Date,data2$Time)
 data2$time2 <- strptime(data2$time2, "%d/%m/%Y %H:%M:%S", tz = "UTC")
-
-
-##Changing datatype for $Date/$Time
-data2[,1] <- as.Date(data2$Date, "%d/%m/%Y")
-data2[,2] <- sub(".*\\s+", "",  strptime(data2$Time, "%H:%M:%S", tz = "UTC"))
 
 ##Changing other columm into numeric;
 
 for(i in 3:9){
     data2[,i] <- as.numeric(data2[,i])
 }
+
 
 ###########################################################################
 #Q1
